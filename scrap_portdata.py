045cf932ec5de3f50a58ad6eefbc25abcfb9ad1e
arulthileeban <arulthileeban023@gmail.com>
2017-12-19 23:18:25 +0530

    Added scraping script for getting data about ports

---
+++ scrap_portdata.py
@@ -0,0 +1,12 @@
+import requests
+from bs4 import BeautifulSoup
+
+WIKI_URL = "https://en.wikipedia.org/wiki/List_of_TCP_and_UDP_port_numbers"
+
+req = requests.get(WIKI_URL)
+soup = BeautifulSoup(req.content, 'lxml')
+table_classes = {"class": ["sortable", "plainrowheaders"]}
+wikitables = soup.findAll("table", table_classes)
+
+print(wikitables)
+
